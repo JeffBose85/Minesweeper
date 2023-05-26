f733067269ddd6fa76c99c8f8256ed5cb3ec6601
Jeffrey Bose <jbose8500@gmail.com>
2023-05-26 15:00:52 -0500

    Added current files

---
+++ js/Tile.js
@@ -0,0 +1,108 @@
+class Tile {
+
+    constructor(x,y) {
+    this.revealed = false;
+    this.mine = false;
+    this.flagged = false;
+    this.x = x;
+    this.y = y;
+    this.id = this.x + "_" + this.y;
+    this.NEIGHBORS = [];
+    this.neighborCount = null;
+    this.neighborMines = 0;
+    this.element = null;
+
+    this.colors = {
+        1:'blue',
+        2:'green',
+        3:'red',
+        4:'purple',
+        5:'maroon',
+        6:'#fa11e7',
+        7:'navy',
+        8:'orange',
+    };
+    }
+
+    ToggleFlagged() {
+        this.flagged = !(this.flagged);
+        if(this.mine && this.flagged) {
+            console.log("Revealed Mine");
+            this.revealed = true;
+        }
+        if(this.mine && !this.flagged) {
+            console.log("Unrevealed Mine");
+            this.revealed = false;
+        }
+    }
+
+    FindNeighbors(tiles) {
+        /*
+            Top Left: x - 1, y + 1
+            Mid Left: x - 1, y
+            Bot Left: x - 1, y - 1
+            Top Mid: x, y + 1
+            Bot Mid: x, y - 1
+            Top Right: x + 1, y + 1
+            Mid Right: x + 1, y
+            Bot Right: x + 1, y - 1
+        */
+        this.NEIGHBORS = [];
+        this.neighborMines = 0;
+        for (let i = -1; i < 2; i++) {
+            for (let j = -1; j < 2; j++) {
+                var id = (this.x + i) + "_" + (this.y + j);
+
+                if(id !== this.id && tiles[id] !== undefined) {
+                    this.NEIGHBORS.push(tiles[id]);
+                    if (tiles[id].mine) {
+                        this.neighborMines += 1;
+                    }
+                }
+
+            }    
+        }
+        this.neighborCount = this.NEIGHBORS.length;
+    }
+
+    GetNeighbors() {
+        if(this.NEIGHBORS.length === 0) {
+            return null;
+        }
+        return this.NEIGHBORS;
+    }
+
+    Reveal(tiles) {
+        this.revealed = true;
+        this.element.classList.remove('unrevealed');
+
+        if(this.mine) {
+            this.element.classList.add('mine');
+            return true;
+        }
+
+        if(this.neighborMines !== 0) {
+            this.element.innerHTML = "<p>" + this.neighborMines + "</p>";
+            this.element.style.color = this.colors[this.neighborMines];
+        } else {
+            this.RevealZeros(tiles);
+        }
+        if(CheckWin()) {
+            Win();
+        }
+    }
+
+    RevealZeros(tiles) {
+        this.NEIGHBORS.forEach(neighbor => {
+            if(neighbor.NEIGHBORS.length === 0) {
+                neighbor.FindNeighbors(tiles);
+                if(!(neighbor.revealed)) {
+                    setTimeout(function() {
+                        neighbor.Reveal(tiles);
+                    }, 50);
+                }
+            }
+        });
+    }
+
+}
---
+++ js/controls.js
@@ -0,0 +1,308 @@
+
+const root = document.querySelector(':root');
+var rootStyle;
+var tileSize;
+var fontSize;
+var tiles = {};
+var tileCount = 0;
+var flagging = false;
+var gameOver = true;
+
+var playsettingsdiv;
+var popupdiv;
+var actiondiv;
+
+
+function StartUp() {
+    ReadStyle();
+
+    playsettingsdiv = document.getElementById('playsettings');
+    popupdiv = document.getElementById('gamepopup');
+    actiondiv = document.getElementById('actions');
+
+    Toggle(false, actiondiv);
+    Toggle(false, popupdiv);
+    SetBoard();
+}
+
+function CheckValues(element, value) {
+    const max = element.getAttribute('max');
+    const min = element.getAttribute('min');
+
+    if(value > max || value < min) {
+        if(document.getElementById('btnPlay') !== null) {
+            document.getElementById('btnPlay').remove();
+        }
+        element.classList.add("error");
+    } else {
+        if(document.getElementById('btnPlay') === null) {
+            var newE = document.getElementById('play').appendChild(document.createElement('button'));
+            newE.setAttribute('id','btnPlay');
+            newE.setAttribute('onclick','StartGame()');
+            newE.innerHTML = "Play";
+            element.classList.remove("error");
+        }
+        
+        SetBoard();
+    }
+}
+
+function ChangeSize(width, height) {
+    var newBoardWidth = width * tileSize;
+    var newBoardHeight = height * tileSize;
+    var sizes = CreateSize(newBoardWidth, newBoardHeight);
+
+    root.style.setProperty('--gameWidth', sizes[0]);
+    root.style.setProperty('--gameHeight', sizes[1]);
+    ReadStyle();
+}
+
+function ZoomControl(control) {
+    x = document.getElementById('txtWidth').value;
+    y = document.getElementById('txtHeight').value;
+    var newTileSize = tileSize;
+
+    if(control === 'zoomin') {
+        newTileSize = (tileSize + 10);
+        if(newTileSize > 80) {
+            return;
+        }
+    }
+    if(control === 'zoomout') {
+        newTileSize = (tileSize - 10);
+        if(newTileSize < 30) {
+            return;
+        }
+    }
+    if(control === 'reset') {
+        newTileSize = 50;
+    }
+    var newFontSize = (newTileSize / (72/96)) - 25;
+
+    root.style.setProperty('--tileSize', newTileSize + "px");
+    root.style.setProperty('--fontSize', newFontSize + "pt");
+    ReadStyle();
+    ChangeSize(x,y);
+}
+
+var keyDown = false;
+
+addEventListener('keydown', function(e) {
+    if(e.code === "Space" && e.target === document.body) {
+        e.preventDefault();
+      }
+    if(e.code === "Space" && !(keyDown)) {
+        keyDown = true;
+        SetFlagging();
+    }
+    if(e.code === "KeyR" && !(keyDown)) {
+        keyDown = true;
+        Loss();
+    }
+})
+
+addEventListener('keyup', function(e) {
+    keyDown = false;
+});
+
+function SetFlagging() {
+    element = document.getElementById('flag');  
+    flagging = !flagging;
+    element.classList.toggle('flagactive');
+}
+
+function CreateSize(width, height) {
+    var newWidth = width + "px";
+    var newHeight = height + "px";
+    return [newWidth, newHeight];
+}
+
+function ReadStyle() {
+    try {
+        rootStyle = getComputedStyle(root);
+        tileSize = parseInt(rootStyle.getPropertyValue('--tileSize').replace("px", ""));
+        fontSize = parseInt(rootStyle.getPropertyValue('--fontSize').replace("px", ""));
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+function StartGame() {
+    gameOver = false;
+    Toggle(false, playsettingsdiv);
+    Toggle(false, popupdiv);
+    Toggle(true, actiondiv);
+    SetBoard();
+}
+
+function SetBoard() {
+    var tileColumnCount = document.getElementById('txtWidth').value;
+    var tileRowCount = document.getElementById('txtHeight').value;
+
+    tileCount = tileColumnCount * tileRowCount;
+
+    CreateTiles(tileColumnCount,tileRowCount);
+    SetMines(tileColumnCount, tileRowCount);
+}
+
+function CreateTiles(x,y) {
+    tiles = {};
+    document.getElementById('gamearea').innerHTML = '';
+
+    for (let i = 0; i < y; i++) {
+        for (let j = 0; j < x; j++) {
+            newTile = new Tile(j+1,i+1);
+            newKey = (j+1) + "_" + (i+1);
+            tiles[newKey] = newTile;
+            DrawTile(newKey);
+        }
+    }
+
+    ChangeSize(x,y);
+}
+
+function SetMines(width, height) {
+    const difficulty = document.getElementById('difficulty').value;
+    
+    const mineCount = Math.floor((tileCount / 9) * (difficulty / 1.1));
+
+    for (let i = 0; i < mineCount; i++) {
+        var x = Math.floor(Math.random() * width) + 1;
+        var y = Math.floor(Math.random() * height) + 1;
+
+        var id = (x) + "_" + (y);
+        if(tiles[id].mine) {
+            i--;
+        } else {
+            tiles[id].mine = true;
+        }
+    }
+
+}
+
+function DrawTile(key) {
+    const gamearea = document.getElementById('gamearea');
+
+    var newTile = gamearea.appendChild(document.createElement('div'));
+    tiles[key].element = newTile;
+    newTile.setAttribute('id',key);
+    newTile.setAttribute('onclick','TileClicked(this.id)');
+    newTile.classList.add('tile');
+    newTile.classList.add('unrevealed');
+    // newTile.innerHTML = key;
+}
+
+function TileClicked(id) {
+    const tile = tiles[id];
+
+    if(!gameOver) {
+        if(!(flagging)) {
+            //REVEAL TILE
+            if(!(tile.revealed || tile.flagged)) { 
+    
+                //CHECK IF MINE
+                if(tile.mine) {
+                    tile.Reveal();
+                    Loss();
+                    return;
+                }
+    
+                //FIND NEIGHBORS
+                tiles[id].FindNeighbors(tiles);
+    
+                //SET TILE NUMBER -> NEIGHBORS W/ MINES
+                tile.Reveal(tiles);
+    
+            }
+        } else {
+            if(!tile.flagged && !tile.revealed) {
+                tile.ToggleFlagged();
+                tile.element.classList.add('flagged');
+            } else {
+                tile.ToggleFlagged();
+                tile.element.classList.remove('flagged');
+            }
+        }
+        if(CheckWin()) {
+            Win();
+        }
+    }
+
+}
+
+function Loss() {
+    gameOver = true;
+    Toggle(true, popupdiv);
+    Toggle(false, actiondiv);
+    PopulatePopup("You Lost");
+    RevealAll();
+}
+
+function Win() {
+    gameOver = true;
+    Toggle(true, popupdiv);
+    Toggle(false, actiondiv);
+    PopulatePopup("You Win!");
+    RevealAll();
+}
+
+function PlayAgain() {
+    Toggle(true, playsettingsdiv);
+    Toggle(false, popupdiv);
+    PopulatePopup("");
+    SetBoard();
+}
+
+function Toggle(show, element) {
+    try {
+        if(show) {
+            element.style.display = 'block';
+        } else if(!show) {
+            element.style.display = 'none';
+        } else {
+            console.error('Unknown arg[0]: Toggle()');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function PopulatePopup(message) {
+    popuptext = document.getElementById('gamepopuptext');
+
+    popuptext.innerHTML = message;
+}
+
+function CheckWin() {
+    if(!gameOver) {
+        for(var tile in tiles) {
+            var currTile = tiles[tile];
+            if((!currTile.revealed && currTile.mine)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+function RevealAll() {
+
+    for(var tile in tiles){
+        tiles[tile].FindNeighbors(tiles);
+        tiles[tile].Reveal(tiles);
+    }
+    Toggle(false, actiondiv);
+}
+
+//DEBUG FUNCTIONS
+function PrintTiles() {
+    console.log(tiles);
+}
+
+function CheckCheckWin() {
+    if(CheckWin()) {
+        console.log("WIN");
+    }
+}
